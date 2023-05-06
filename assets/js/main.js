714884f3b62f311f2d0bb2f39c734d7222a0938c
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-05-06 18:19:04 -0300

    Continue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('linkado')
+const keyWeather = '38d01c23be97354a5a86c28153befa3b'
+const apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${keyWeather}&lang=pt_br`
+
+console.log(apiURL)
+
+const form = document.querySelector('#city')
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    console.log('Correto')
+})
