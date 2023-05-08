763f32d0e9e834f45c20474e75d4426335ea1860
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-05-08 18:11:33 -0300

    continue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
-const keyWeather = '38d01c23be97354a5a86c28153befa3b'
-const apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${keyWeather}&lang=pt_br`
+const keyWeather1 = '38d01c23be97354a5'
+const keyWeather2 = 'a86c28153befa3b'
+const apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${keyWeather1+keyWeather2}&lang=pt_br`
 
-console.log(apiURL)
 
 const form = document.querySelector('#city')
 
 form.addEventListener('submit', (event)=>{
     event.preventDefault()
-    console.log('Correto')
+    const input = document.querySelector('.pesquisa').value
+    console.log(input)
 })
