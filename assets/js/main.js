d8b8e1e2a847055cc2a3ef70db592e3b7574e95f
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-05-12 18:24:05 -0300

    Corrige endereço API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,27 @@
 const keyWeather1 = '38d01c23be97354a5'
 const keyWeather2 = 'a86c28153befa3b'
-const apiURL = ``
 
+const cidade = document.querySelector('.cidade')
+const temperatura = document.querySelector('.temperatura')
+const maxTemp = document.querySelector('.max-temperatura')
+const minTemp = document.querySelector('.min-temperatura')
 
 const form = document.querySelector('#city')
 
+
 form.addEventListener('submit', (event)=>{
     event.preventDefault()
-
     const input = document.querySelector('.pesquisa').value
-    
-    fetchAPI(encodeURI(input))
+    if(input!=''){
+        fetchAPI(encodeURI(input))
+    }else{
+        console.log('Digite uma cidade')
+    }
 })
 
 async function fetchAPI(city){
-    const consulta = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${keyWeather1+keyWeather2}&lang=pt_br`)
+    const consulta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyWeather1+keyWeather2}&lang=pt_br`)
     const resultado = await consulta.json()
-
 
     console.log(resultado)
 }
